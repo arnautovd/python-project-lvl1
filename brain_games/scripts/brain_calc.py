6d8399c305a2f9afcdddbfbdca75e2d00011aee6
arnautovd <arnautov.dmitry2015@yandex.ru>
2022-07-27 17:13:56 +0300

    rebuild brain_calc

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -2,12 +2,15 @@ import prompt
 import random
 
 
-def main():  # noqa: C901
+def greet():
     print("Welcome to the Brain Games!")
     user_name = prompt.string("May I have your name? ")
     print(f"Hello, {user_name}!")
     print("Answer \"yes\" if the number is even, otherwise answer \"no\".")
     correct_answers = 0
+
+def main():  # noqa: C901
+    greet()
     while correct_answers < 3:
 
         num1 = int(random.random() * 100)
