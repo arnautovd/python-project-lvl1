a81c09fbdeb9ef0ce2b5b907019b305ba6f95f59
arnautovd <arnautov.dmitry2015@yandex.ru>
2022-07-27 10:28:47 +0300

    checking linter and correcting some mistakes

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,18 +1,19 @@
 import prompt
 import random
 
-def main():
+
+def main():  # noqa: C901
     print("Welcome to the Brain Games!")
     user_name = prompt.string("May I have your name? ")
     print(f"Hello, {user_name}!")
-    print(f"Answer \"yes\" if the number is even, otherwise answer \"no\".")
+    print("Answer \"yes\" if the number is even, otherwise answer \"no\".")
     correct_answers = 0
     while correct_answers < 3:
 
         user_number = int(random.random() * 100)
         print(f"Question: {user_number}")
         user_answer = prompt.string("Your answer: ")
-    
+
         if user_number % 2 == 0 and user_answer == 'yes':
             print('Correct!')
             correct_answers += 1
@@ -21,10 +22,12 @@ def main():
             correct_answers += 1
         else:
             if user_answer == "yes":
-                print(f"{user_answer} is wrong answer ;(. Correct answer was 'no'.")
+                print(f"""{user_answer} is wrong answer
+                ;(. Correct answer was 'no'.""")
                 print(f"Let's try again, {user_name}!")
             elif user_answer == "no":
-                print(f"{user_answer} is wrong answer ;(. Correct answer was 'yes'.")
+                print(f"""{user_answer} is wrong answer
+                ;(. Correct answer was 'yes'.""")
                 print(f"Let's try again, {user_name}!")
             elif user_answer != "yes" and user_answer != "no":
                 print(f"{user_answer} is wrong answer")
